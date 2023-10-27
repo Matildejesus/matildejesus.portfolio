59b545d8381c01f7ba271b089c5677f798232257
Matildejesus <malvesdejesu@student.unimelb.edu.au>
2023-10-27 15:30:50 +1100

    migrated to scss

--- portfolio.js
+++ portfolio.js
@@ -4,20 +4,6 @@ import bodyParser from "body-parser";
 const app = express();
 const port = process.env.PORT || 3000;
 
-var week = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = "";
-let date = "";
-var tasks = [];
-var dayTasks = [];
-
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
@@ -38,58 +24,6 @@ app.get("/work", (req, res) => {
 
 app.get("/resume", (req, res) => {
   res.render("resume", { activeTab: "resume" });
-});
-
-function findDate(req, res, next) {
-  const d = new Date();
-  day = d.getDay();
-  date = d.toLocaleDateString();
-  next();
-}
-app.use(findDate);
-
-function getNewTask(req, res, next) {
-  if (req.body["newTask"]) {
-    tasks.push(req.body["newTask"]);
-  }
-  next();
-}
-app.use(getNewTask);
-
-app.get("/dayTask", (req, res) => {
-  res.render("daytask.ejs", {
-    taskPage: "dayTask",
-    dayOfTheWeek: week[day],
-    todayDate: date,
-    newTask: dayTasks,
-  });
-});
-
-app.post("/dayTask", (req, res) => {
-  for (const task of tasks) {
-    dayTasks.push(task);
-  }
-  res.render("daytask.ejs", {
-    taskPage: "dayTask",
-    dayOfTheWeek: week[day],
-    todayDate: date,
-    newTask: dayTasks,
-  });
-  console.log(dayTasks);
-  if (tasks.length == 1) {
-    tasks = [];
-  }
-});
-
-app.post("/clearArray", (req, res) => {
-  tasks = [];
-  dayTasks = [];
-  res.render("daytask.ejs", {
-    taskPage: "dayTask",
-    dayOfTheWeek: week[day],
-    todayDate: date,
-    newTask: tasks,
-  });
 });
 
 app.listen(port, () => {
